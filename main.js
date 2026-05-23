0f36b84657c3dc18cd47777cbaf938825f0e449c
Isabel Motta <isamotta2003@gmail.com>
2026-05-23 14:01:23 -0300

    validacaoContato

--- main.js
+++ main.js
@@ -48,21 +48,6 @@ window.addEventListener('scroll', () => {
   }
 });
 
-/* ── Menu Hamburguer Mobile Funcional ── */
-const botao = document.getElementById("botao-hamburguer");
-const menu = document.getElementById("menu-mobile");
-
-botao.addEventListener("click", () => {
-
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-    menu.style.position = "fixed";
-  }
-
-});
-
 /* ── canvas de partículas (não espero que ninguem entenda mexi tanto nisso que nem eu entendo mais) ── */
 const canvasElem = document.getElementById('tela-particulas');
 if (canvasElem) {
@@ -125,3 +110,44 @@ if (canvasElem) {
   /* Inicialização do ciclo de renderização */
   rodarCiclo();
 }
+
+/* ── Menu Hamburguer Mobile Funcional ── */
+const botao = document.getElementById("botao-hamburguer");
+const menu = document.getElementById("menu-mobile");
+
+botao.addEventListener("click", () => {
+
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+    menu.style.position = "fixed";
+  }
+
+});
+
+/* Validação do formulário*/
+
+const formulario = document.getElementById("form-contato-web");
+
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+const mensagem = document.getElementById("mensagem");
+
+formulario.addEventListener("submit", (event) => {
+
+  event.preventDefault();
+
+  if (nome.value === "") {
+    alert("Preencha o nome");
+  }
+
+  if (email.value === "") {
+    alert("Preencha o email");
+  }
+
+  if (mensagem.value === "") {
+    alert("Preencha a mensagem");
+  }
+
+});
